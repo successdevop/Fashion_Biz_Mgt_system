34bd110e0eda494ea137bd3713f60a9537266f9d
successdevop <successraphael28@gmail.com>
2026-05-14 19:14:02 +0100

    taught my student how to connect their flask application to sqlite database

---
+++ db.py
@@ -0,0 +1,9 @@
+# import sqlite3
+#
+# connection = sqlite3.connect("test.db")
+# cursor = connection.cursor()
+# cursor.execute('''CREATE TABLE IF NOT EXIST Employee
+#                   (emp_id INT, Name TEXT, position TEXT)''')
+#
+# connection.commit()
+# connection.close()
--- main.py
+++ main.py
@@ -1,17 +1,54 @@
-from flask import Flask, jsonify
+# from _testcapi import error
+# from flask import Flask, jsonify, request
+#
+#
+# app = Flask(__name__)
+#
+# @app.route("/")
+# def hello_world():
+#     return "hello world", 200
+#
+# @app.route("/simple")
+# def simple():
+#     return jsonify(text="Simple is the new standard. So watch out",
+#                    message="Testing my flask api")
+#
+# @app.route("/not_found")
+# def not_found():
+#     return jsonify(error="not found"), 404
+#
+#
+# @app.route("/parameter")
+# def parameter():
+#     name = request.args.get("name")
+#     age = int(request.args.get("age", 0))
+#     if age > 18:
+#         return f"Congratulations {name}, You are eligible"
+#     else:
+#         return f"sorry {name}, You are not eligible to access this API"
+#
+#
+# @app.route("/api_variable/<string:name>/<int:age>")
+# def api_variable(name: str, age: int):
+#     if age > 18:
+#         return f"Congratulations {name}, you are eligible"
+#     else:
+#         return f"Sorry {name}, you are not allowed to use this API at this age"
+
+# if __name__ == "__main__":
+#     app.run()
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+engine = create_engine("sqlite:///test.db")
+Base = declarative_base()
 
 
-app = Flask(__name__)
+class Employee(Base):
+    __tablename__ = "employee"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    position = Column(String)
 
-@app.route("/")
-def hello_world():
-    return "hello world"
-
-@app.route("/simple")
-def simple():
-    return jsonify(text="Simple is the new standard. So watch out",
-                   message="Testing my flask api")
-
-if __name__ == "__main__":
-    app.run()
-
+Base.metadata.create_all(engine)
